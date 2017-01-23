c2d45d19e0d1db39cb199e680b46fe067eb649b5
Saïd Ibrihen <said026@gmail.com>
2017-01-23 14:59:22 +0100

    added graph update every 3 seconds

--- vote-dashboard/refresh.js
+++ vote-dashboard/refresh.js
@@ -35,48 +35,64 @@ var init = [
 
 graph = new Chart(ctx).Doughnut(init, options);
 
-var results;
+/* Makes a scan of the Cloudant database to retrieve the votes and update the chart */
+function getData() {
+	
+	var results;
+	$.ajax({
+	  url: "https://2668c395-f5fb-4839-94f7-5a448e2cb674-bluemix.cloudant.com/voteapp-aggregates/ec-voteapp-aggregates",
+	  type: 'GET',
+	  dataType: 'json',
+	  contentType: 'text/plain',
+	  error: function(status) {
+	    console.log("Error in accessing Cloudant database");
+	    console.log(status);
+	  },
+	  success: function(data) {
+	  	/* Retrieving the number of votes for each color */
+	    results = data["results"];
+	    var redCount = results["RED"];
+	    var greenCount = results["GREEN"];
+	    var blueCount = results["BLUE"];
+	
+	    var data = [
+	      {
+	          value: redCount,
+	          color:"#e74c3c",
+	          highlight: "#c0392b",
+	          label: "Red"
+	      },
+	      {
+	          value: greenCount,
+	          color: "#2ecc71",
+	          highlight: "#27ae60",
+	          label: "Green"
+	      },
+	      {
+	          value: blueCount,
+	          color: "#3498db",
+	          highlight: "#2980b9",
+	          label: "Blue"
+	      }
+	    ];
+		
+		if (graph.segments[0].value != data[0].value ||
+            graph.segments[1].value != data[1].value ||
+            graph.segments[2].value != data[2].value) {
+            	// Only update if we have new value(s) 
+			    graph.segments[0].value = data[0].value;
+			    graph.segments[1].value = data[1].value;
+			    graph.segments[2].value = data[2].value;
+			    graph.update();
+            }
+	  },
+	});
+}
 
-$.ajax({
-  url: "https://2668c395-f5fb-4839-94f7-5a448e2cb674-bluemix.cloudant.com/voteapp-aggregates/ec-voteapp-aggregates",
-  type: 'GET',
-  dataType: 'json',
-  contentType: 'text/plain',
-  error: function(status) {
-    console.log("Error");
-    console.log(status);
-  },
-  success: function(data) {
-    results = data["results"];
-    console.log(results);
-    var redCount = results["BLUE"];
-    var greenCount = results["RED"];
-    var blueCount = results["GREEN"];
-
-    var data = [
-      {
-          value: redCount,
-          color:"#e74c3c",
-          highlight: "#c0392b",
-          label: "Red"
-      },
-      {
-          value: greenCount,
-          color: "#2ecc71",
-          highlight: "#27ae60",
-          label: "Green"
-      },
-      {
-          value: blueCount,
-          color: "#3498db",
-          highlight: "#2980b9",
-          label: "Blue"
-      }
-    ];
-
-    graph.segments[0].value = data[0].value;
-    graph.segments[1].value = data[1].value;
-    graph.segments[2].value = data[2].value;
-    graph.update();
-  },
+/* This functions calls getData() function every 3 seconds */
+$(function() {
+  getData();
+  $.ajaxSetup({ cache: false });
+  /* Get the data every 3 seconds */
+  setInterval(getData, 3000);
 });
